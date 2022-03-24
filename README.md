7d6b3a54c6e021aa22f5b45e64751025cf148ebd
Fredrik Burmester <fredrik.burmester@gmail.com>
2022-03-24 09:53:26 +0100

    refactoring

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
 # photos-to-json
-A python script that scans all photos in a directory and creates a neat JSON file from it where each folder is an album. 
 
-This script is created for photographers and/or web developers who organize their photos into folders and want to keep track of them and/or display them on a website. 
+A python script that scans all photos in a directory and creates a neat JSON file from it where each folder is an album.
+
+This script is created for photographers and/or web developers who organize their photos into folders and want to keep track of them and/or display them on a website.
 
 This json file can come in handly when loading images on a website since websites like to know the width and height of a photo before the image has been loaded to prevent content shift. It also comes in handy when lazy loading images, since you'll know which image is next. The script also computes if it's a portrait or landscape image, which can also help when designing a website or grid of images.
 
-For a real world example, see https://fredrik.studio
+For a real world example, see <https://fredrik.studio>
 
 ```
 Directory
@@ -18,6 +19,7 @@ Directory
 ```
 
 This python program will scan a folder and all its subfolders, look for all files with a jpg file extention and then create a JSON file with the following structure:
+
 ```
 [{
   title ""
@@ -28,9 +30,14 @@ This python program will scan a folder and all its subfolders, look for all file
   y = <height in px>
 }]
 ```
-The exported filename is `photos.json`. The export path is hardcoded but is easily changed. 
+
+The exported filename is `photos.json`.
+
+Usage: `photos-to-json.py <photosDirectory> <jsonDirectory>`
+Example: `photos-to-json.py /User/john/images /User/john/json`
 
 **Note:** If the file `photos.json` already exist the exact path, then that files content will be copied into the new file and the existing information about a certain file will not be overwritten. This means that if you were to change a files name or resolution etc. then that will be carried over to the new file.  
 
 ## To-do
+
 - Break up file into functions
--- photos-to-json.py
+++ photos-to-json.py
@@ -5,67 +5,125 @@ from PIL import Image
 from os import walk
 from natsort import natsorted, ns
 
-j = open("/unraid-data/appdata/NginxProxyManager/static/json/photos.json",) 
-data = json.load(j) 
-g = open("/unraid-data/appdata/NginxProxyManager/static/json/photos.json", 'w')
-sys.stdout = g
+try:
+    if len(sys.argv) != 3:
+        print("Usage: photos-to-json.py <photosDirectory> <jsonDirectory>")
+        sys.exit(1)
+    photosDirectory = sys.argv[1]
+    jsonDirectory = sys.argv[2] + '/photos.json'
+    if not os.path.isdir(photosDirectory) and not os.path.isdir(jsonDirectory):
+        print("Directory not found. Please check the path.")
+        sys.exit(1)
+except:
+    print("Missing agruments. Usage: photos-to-json.py <photosDirectory> <jsonDirectory>")
+    sys.exit(1)
 
-mypath = "/unraid-data/appdata/NginxProxyManager/static/images"
+
+# try to open json file, if fail, create it
+try:
+    print("Trying to find existing json file...")
+    with open(jsonDirectory, "r") as file:
+        data = json.load(file)
+    print("Found existing json file.")
+except:
+    print("[Error] Json file not found. Creating new one...")
+    with open(jsonDirectory, 'w') as outfile:
+        outfile.write('[]')
+    
+    try:
+        print("Trying again...")
+        with open(jsonDirectory, "r") as file:
+            data = json.load(file)
+        print("Found json file.")
+    except:
+        print("[Error] Something went wrong. Please check the path.")
+        sys.exit(1)
+
+print("\nReading photos from " + photosDirectory)
+print("Writing json to " + jsonDirectory)
+
+g = open(jsonDirectory, 'w') # open json file
+
+console = sys.stdout
+sys.stdout = g # redirect output to json file
 
 f = []
 id = 0
-firstTime = 0
 ignored_files= [".DS_Store"]
 
 print('[')
-for dirname in walk(mypath):
+sys.stdout = console # redirect output back to console
+
+def replaceUTFCharacters(s):
+    s.replace("\u00f6", "o\u0308") # ö
+    s.replace("\u00D6", "O\u0308") # Ö
+    s.replace("\u00E4", "a\u0308") # ä
+    s.replace("\u00C4", "A\u0308") # Ä
+    return s
+
+def getBackUTFCharacters(s):
+    s.replace("o\u0308", "\u00f6") # ö
+    s.replace("O\u0308", "\u00D6") # Ö
+    s.replace("a\u0308", "\u00E4") # ä
+    s.replace("A\u0308", "\u00C4") # Ä
+    return s
+
+def getOrientation(i):
+    if i.size[0] / i.size[1] > 1: 
+        return 'wide'
+    else:
+        return 'tall'
+
+
+for dirname in walk(photosDirectory):
     for album in dirname[1]:
-        album.replace("\u00f6", "o\u0308") # ö
-        album.replace("\u00D6", "O\u0308") # Ö
-        album.replace("\u00E4", "a\u0308") # ä
-        album.replace("\u00C4", "A\u0308") # Ä
-        currentImage = []
-        for image in walk(mypath + '/' + album):
-            counter = 0
-            for i in image[2]:
-                if i not in ignored_files:
-                    name = i.split('.')[0]
-                    if len(name) > 0:
-                        currentImage.append(name)
-                        counter += 1
-            currentImage = natsorted(currentImage, reverse=True)
-            counter = 0
-            for i in currentImage:
-                currentImage[counter] = str(currentImage[counter]) + '.jpg'
-                counter += 1
-            for filename in currentImage:
-                im = Image.open(mypath + '/' + album + '/' + filename)
-                display = ''
-                if im.size[0]/im.size[1] > 1: 
-                  display = 'wide'
-                else:
-                  display = 'tall'
-                for(x) in (data):
-                    if(x['filename'] == filename and x['album'] == album):
-                        title = x['title']
-                        display = x['display']
-                album = album.replace("o\u0308","\u00f6")
-                jsonitem = {
-                    'id': id,
-                    'title': '',
-                    'album': str(album),
-                    'filename': filename,
-                    'display': display,
-                    'x': im.size[0],
-                    'y': im.size[1]
-                }
-                id += 1
-                student_dumped = json.dumps(jsonitem)
-                if firstTime == 1:
-                  print(',')
-                print(student_dumped)
-                firstTime = 1
-            break
-        
-    break
+        album = replaceUTFCharacters(album)
+
+        sys.stdout = console
+        print("Processing: " + album)
+
+        albumImages = []
+        for image in walk(photosDirectory + '/' + album):
+            # Get filename
+            for filename in image[2]: 
+                print(" - " + filename)
+                if filename not in ignored_files:
+                    filename = replaceUTFCharacters(filename)
+                    albumImages.append(filename)
+                else: 
+                    print("   - [Ignored] " + filename)
+
+            albumImages = natsorted(albumImages, reverse=True)
+
+        for filename in albumImages:
+            im = Image.open(photosDirectory + '/' + album + '/' + filename)
+
+            orientation = getOrientation(im)
+
+            for(x) in (data):
+                if(x['filename'] == filename and x['album'] == album):
+                    title = x['title']
+                    orientation = x['display']
+
+            album = getBackUTFCharacters(album)
+
+            jsonitem = {
+                'id': id,
+                'title': '',
+                'album': str(album),
+                'filename': filename,
+                'display': orientation,
+                'x': im.size[0],
+                'y': im.size[1]
+            }
+
+            student_dumped = json.dumps(jsonitem)
+
+            sys.stdout = g # redirect output to json file
+            
+            if id != 0:
+                print(',')
+            id += 1
+
+            print(student_dumped)
 print(']')
